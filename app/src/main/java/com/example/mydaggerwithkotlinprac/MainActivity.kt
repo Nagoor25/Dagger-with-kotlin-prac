31aaf7f10b2dc5e45542ab33cfa16a24adde4948
Nagoor25 <nagoorvali25@gmail.com>
2022-03-03 15:23:30 +0530

    2-Di using filed injection and inject method

--- app/src/main/java/com/example/mydaggerwithkotlinprac/MainActivity.kt
+++ app/src/main/java/com/example/mydaggerwithkotlinprac/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.mydaggerwithkotlinprac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
     lateinit var mobile: Mobile
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +18,13 @@ class MainActivity : AppCompatActivity() {
         mobile.ringing()
         */
         // using Dagger di DaggerMobileComponent created  by dagger
+        /*
         mobile=DaggerMobileComponent.create().getMobile()
+        mobile.ringing()
+        */
+        val mobileComponent=DaggerMobileComponent.create()
+        // By using this implement all the depend parameter with inject
+        mobileComponent.inject(this)
         mobile.ringing()
 
     }
--- app/src/main/java/com/example/mydaggerwithkotlinprac/MobileComponent.kt
+++ app/src/main/java/com/example/mydaggerwithkotlinprac/MobileComponent.kt
@@ -1,9 +1,12 @@
 package com.example.mydaggerwithkotlinprac
 
 import dagger.Component
+import javax.inject.Inject
 
 @Component
 interface MobileComponent {
 
     fun getMobile():Mobile
+
+    fun inject(mainActivity: MainActivity)
 }
